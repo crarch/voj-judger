a6a781c213e1af45315b38333501fc6329feb9b9
hilaolu <i@phyzait.moe>
2021-07-19 02:20:44 +0800

    add reconnect

--- src/main.rs
+++ src/main.rs
@@ -31,40 +31,44 @@ async fn main(){
     
     let judger_key=get_env("JUDGER_KEY");
     
-    let _workers=get_env("WORKERS").parse::<usize>().unwrap();
-
     let ws_url="ws".to_string()+&get_env("API_URL")[4..]+"/websocket";
-
-    let mut client=ClientBuilder::new(&ws_url)
-        .unwrap();
-        
-    client.add_header("Authorization".to_string(),judger_key);
     
-    let mut client=client.async_connect()
-        .await
-        .unwrap();
+    
+    loop{
+        let mut client=ClientBuilder::new(&ws_url)
+            .unwrap();
+            
+        client.add_header("Authorization".to_string(),judger_key.clone());
+        
+        if let Ok(mut client)=client.async_connect().await{
 
+            println!("WebSocket Connection Established");
+            
+            while let Some(Ok(message))=client.next().await{
+                match message.opcode(){
+                    Opcode::Text=>{
+                        let data=message.as_text().unwrap();
 
-    while let Some(Ok(message))=client.next().await{
-        match message.opcode(){
-            Opcode::Text=>{
-                let data=message.as_text().unwrap();
+                        if let Some((job_id,question_id,user_id))=parse_job(data).await{
+                            println!("judging {}",&job_id);
+                            let result=judge(&job_id,question_id,user_id).await.unwrap();
+                            let result:Value=Bson::from(result).into();
+                            let result=result.to_string();
+                            client.send(Message::text(result)).await.unwrap(); 
+                        }
 
-                if let Some((job_id,question_id,user_id))=parse_job(data).await{
-                    println!("judging {}",&job_id);
-                    let result=judge(&job_id,question_id,user_id).await.unwrap();
-                    let result:Value=Bson::from(result).into();
-                    let result=result.to_string();
-                    client.send(Message::text(result)).await.unwrap(); 
+                    },
+
+                    Opcode::Ping => client.send(Message::pong(message.into_data())).await.unwrap(),
+
+                    _=>(),
+
                 }
-
-            },
-
-            Opcode::Ping => client.send(Message::pong(message.into_data())).await.unwrap(),
-
-            _=>(),
-
+            }
+        }else{
+            continue;
         }
+        
     }
 }
 

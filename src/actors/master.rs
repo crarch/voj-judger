d3de4a5d4a9c2a7a55299cc493f50dfbf19edad3
hilaolu <i@phyzait.moe>
2021-10-11 23:48:12 +0800

    update

--- src/actors/master.rs
+++ src/actors/master.rs
@@ -17,6 +17,7 @@ use actix::io::SinkWrite;
 use super::WsDisconnect;
 use super::Worker;
 use futures::StreamExt;
+use std::time::{Duration, Instant};
 
 use crate::env::get_env;
 
@@ -170,6 +171,7 @@ impl Handler<SpawnWsClient> for Master{
                     WsClient{
                         framed:SinkWrite::new(sink, ctx),
                         master_addr:master_addr
+                        hb:Instant::now()
                     }
                 });
             }else{

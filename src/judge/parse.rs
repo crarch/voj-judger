efff1aa5c9a59947a55ae197b5f62ff001883fe7
hilaolu <i@phyzait.moe>
2021-11-16 23:19:47 +0800

    update

--- src/judge/parse.rs
+++ src/judge/parse.rs
@@ -304,7 +304,7 @@ pub fn parse(input_vcd:&str)->Option<Document>{
                         let mut words_iter=words.iter();
                         // words_iter.next();
                         
-                        for j in 0..i{
+                        for j in 0..(i-1){
                             if(w[j]=='='){
                                 words_iter.next();
                             }
